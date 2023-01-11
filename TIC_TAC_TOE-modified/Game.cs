fa07297c9fb41e8a80d3cadcf71e89500c387842
Impresia <s.phersashvili@gmail.com>
2023-01-11 12:35:10 +0400

    First commit

---
+++ TIC_TAC_TOE-modified/Game.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIC_TAC_TOE
+{
+    
+    public class Game
+    {
+        Conditions conditions = new Conditions();
+
+        public static  string X { get; } = "X";
+        public static string O { get; } = "O";
+
+        public List<string> squares = new List<string>() { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+
+
+
+        //Player O choosing square !
+        public void chooseSquare_O(List<string> square, int number)
+        {
+            
+            
+              number=  conditions.GetValidNumber(number);
+            
+
+
+
+            while (Conditions.SquareIsOccupied(square, number))
+            {
+                Console.Write("Square is occupied, try another! ");
+                string answer = Console.ReadLine();
+                while (!int.TryParse(answer, out number))
+                {
+                    Console.Write("Please use numbers: ");
+                    answer = Console.ReadLine();
+                }
+                
+                    number = Convert.ToInt32(answer);
+                    number = conditions.GetValidNumber(number);
+                
+                Console.WriteLine();
+            }
+           
+            
+            
+            
+            if (!Conditions.SquareIsOccupied(square, number))
+            {
+
+                square[number] = O;
+                GetBoard();
+
+
+                if (Conditions.HasWon(square, "X"))
+                {
+                    Console.WriteLine("X won!");
+
+                }
+
+                if (Conditions.HasWon(square, "O"))
+                {
+                    Console.WriteLine("O won!");
+
+
+                }
+                if (Conditions.Draw(square))
+                {
+                    Console.WriteLine("It's a draw!");
+
+                }
+
+            }
+
+
+
+        }
+
+        //Player X Choosing square!
+        public void chooseSquare_X(List<string> square, int number)
+        {
+
+            number=conditions.GetValidNumber(number);
+
+
+
+
+            while (Conditions.SquareIsOccupied(square, number))
+            {
+                Console.Write("Square is occupied, try another! ");
+                string answer = Console.ReadLine();
+                while (!int.TryParse(answer, out number))
+                {
+                    Console.Write("Please use numbers: ");
+                    answer = Console.ReadLine();
+                }
+                
+                    number = Convert.ToInt32(answer);
+                   number= conditions.GetValidNumber(number);
+                
+                Console.WriteLine();
+            }
+           
+
+            
+            
+            
+            
+            if (!Conditions.SquareIsOccupied(square, number))
+            {
+
+                square[number] = X;
+                GetBoard();
+
+
+
+                if (Conditions.HasWon(square, "X"))
+                {
+                    Console.WriteLine("X won!");
+                }
+
+                if (Conditions.HasWon(square, "O"))
+                {
+                    Console.WriteLine("O won!");
+                }
+                
+                if (Conditions.Draw(square))
+                {
+                    Console.WriteLine("It's a draw!");
+                }
+
+            }
+
+
+
+        }
+
+
+        public void GetBoard()
+        {
+            Console.WriteLine($" {squares[1]}  | {squares[2]}  | {squares[3]} ");
+            Console.WriteLine(" ---+----+---");
+            Console.WriteLine($" {squares[4]}  | {squares[5]}  | {squares[6]} ");
+            Console.WriteLine(" ---+----+---");
+            Console.WriteLine($" {squares[7]}  | {squares[8]}  | {squares[9]} ");
+        }
+
+
+    }
+}
---
+++ TIC_TAC_TOE-modified/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using TIC_TAC_TOE;
+
+
+
+Game newGame = new Game();
+Console.WriteLine($" {1}  | {2}  | {3} ");
+Console.WriteLine(" ---+----+---");
+Console.WriteLine($" {4}  | {5}  | {6} ");
+Console.WriteLine(" ---+----+---");
+Console.WriteLine($" {7}  | {8}  | {9} ");
+
+Console.WriteLine();
+
+Console.WriteLine("(choose numbers between 1-9 !)");
+
+
+
+
+while (true)
+{
+    Console.Write($"It's {Game.O}-s turn to play which square do you want to play? ");
+    string answer = Console.ReadLine();
+    int result;
+    
+    //Only converting to int if response is number!
+    while(!int.TryParse(answer,out result))
+    {
+        Console.Write("Please use numbers: ");
+        answer = Console.ReadLine();
+    }
+    
+        result = Convert.ToInt32(answer);
+         Console.WriteLine();
+
+
+
+
+
+    newGame.chooseSquare_O(newGame.squares, result);
+    if (Conditions.HasWon(newGame.squares, Game.O) || Conditions.Draw(newGame.squares))
+        break;
+
+
+
+
+
+    Console.Write($"It's {Game.X}-s turn to play which square do you want to play? ");
+    answer = Console.ReadLine();
+
+    //Only converting to int if response is number!
+    while (!int.TryParse(answer, out result))
+    {
+        Console.Write("Please use numbers: ");
+        answer = Console.ReadLine();
+    }
+        result = Convert.ToInt32(answer);
+
+    Console.WriteLine();
+
+
+
+
+
+
+    newGame.chooseSquare_X(newGame.squares, result);
+    if (Conditions.HasWon(newGame.squares, Game.X) || Conditions.Draw(newGame.squares))
+        break;
+
+
+
+}
